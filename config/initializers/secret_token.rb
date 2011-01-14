5d0498df6c356eb53035edc564eb83f7d1087d00
Michael Wynholds <mike@carbonfive.com>
2011-01-13 16:37:58 -0800

    Initial duggit project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Duggit::Application.config.secret_token = '83de91c88a25ade1857236f52956d7f48b32ca8e711e758ac68c3858b2858e9eaab3ed5b7d3f9111eb0939a4dd49267b933522f857f5bd44ffa02647f949e239'
