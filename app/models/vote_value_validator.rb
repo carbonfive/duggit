b13feebcd4f74b3ca1697e2d9f909db577fc3bc7
Michael Wynholds <mike@carbonfive.com>
2011-01-26 15:49:29 -0800

    Delete unused validator.

--- app/models/vote_value_validator.rb
+++
@@ -1,7 +0,0 @@
-class VoteValueValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    if value < -1 || value > 1
-      record.errors[attribute] << "Invalid vote value: #{value}"
-    end
-  end
-end
