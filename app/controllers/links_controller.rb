6617b9009a84cc3cf96ee45b588f965742776636
Jared Carroll <dev+jccarbonfive@carbonfive.com>
2011-01-29 14:40:21 -0800

    Move Homepage Into Links Controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -15,4 +15,11 @@ class LinksController < ApplicationController
     redirect_to root_path
   end
 
+  def index
+    @links = Link.order('created_at DESC').limit(30)
+    @votes = Vote.where(:link_id => @links, :user_id => current_user).each_with_object({}) do |vote, map|
+      map[vote.link_id] = vote
+    end
+  end
+
 end
--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -56,4 +56,32 @@ describe LinksController do
       response.should redirect_to(root_path)
     end
   end
+
+  describe '#index' do
+    context 'given 30+ links in the system' do
+      before do
+        (1..40).each do |i|
+          Link.create :title => i.to_s, :url => 'http://www.carbonfive.com'
+        end
+
+        get :index
+
+        @links = assigns(:links)
+      end
+
+      it 'should get the last 30 links in reverse chronological order' do
+        @links.collect(&:title).should == (11..40).to_a.reverse.collect(&:to_s)
+      end
+
+      it 'should go to the home page' do
+        response.should render_template(:index)
+      end
+
+      it 'is routed to from "/"' do
+        { :get => '/' }.should route_to(:controller => 'links',
+                                        :action => 'index')
+      end
+    end
+  end
+
 end
