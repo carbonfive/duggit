92e373809a72f32ccc32a0fb8bd1a1d5930827fd
Michael Wynholds <mike@carbonfive.com>
2011-02-14 10:20:32 -0800

    Override specific rspec formatter method to handle a nil message in an exception (thanks Cassandra or Thrift)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,3 +28,34 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 end
+
+require 'rspec/core/formatters/base_text_formatter'
+module RSpec
+  module Core
+    module Formatters
+
+      class BaseTextFormatter < BaseFormatter
+
+        private
+
+        def dump_failure(example, index)
+          exception = example.execution_result[:exception]
+          output.puts "#{short_padding}#{index.next}) #{example.full_description}"
+          output.puts "#{long_padding}#{red("Failure/Error:")} #{red(read_failed_line(exception, example).strip)}"
+          output.puts "#{long_padding}#{red(exception.class.name << ":")}" unless exception.class.name =~ /RSpec/
+          exception.message.split("\n").each { |line| output.puts "#{long_padding}  #{red(line)}" } if exception.message
+
+          example.example_group.ancestors.push(example.example_group).each do |group|
+            if group.metadata[:shared_group_name]
+              output.puts "#{long_padding}Shared Example Group: \"#{group.metadata[:shared_group_name]}\" called from " +
+                              "#{backtrace_line(group.metadata[:example_group][:location])}"
+              break
+            end
+          end
+        end
+
+      end
+
+    end
+  end
+end
