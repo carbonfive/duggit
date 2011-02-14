45cf5f88678bba14374dfa4ad3598cb3f2ae0f23
Michael Wynholds <mike@carbonfive.com>
2011-02-14 10:02:14 -0800

    Convert params in votes controller to ints before saving

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,8 +4,8 @@ class VotesController < ApplicationController
 
   def create
     vote = Vote.new :user_id => current_user.id,
-                    :link_id => params[:link_id],
-                    :value => params[:vote][:value]
+                    :link_id => params[:link_id].to_i,
+                    :value => params[:vote][:value].to_i
 
     unless vote.save
       flash[:alert] = "Unable to tally your vote"
